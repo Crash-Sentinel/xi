d7c0e6520a7841ac3d24464c248aff805f05c5ed
Crash Sentinel <151790578+Crash-Sentinel@users.noreply.github.com>
2024-11-06 11:55:00 -0500

    Added Integral.h file Version 1
    Includes a basic Simpson's rule function, plans to make Error based determination of subintervals in the near future if possible

---
+++ include/integral.h
@@ -0,0 +1,46 @@
+#ifndef XI_INTEGRAL
+#define XI_INTEGRAL
+
+#include <omp.h>
+#include <iostream>
+#include "math_consts.h"
+
+namespace xi_integral
+{
+    /*
+        Simpsons Rule Based Definite Integral
+    */
+    template <typename Func, typename T1, typename T2>
+    inline long double definite_integral(Func&& f, T1 a, T2 b, int n = -1)
+    {
+        if (n % 2 == 1)
+        {
+            std::cerr << "N must be even for definite integral!" << std::endl;
+            return -1;
+        }
+
+        if (n == -1) n = 10'000;
+
+        long double delta_x = (static_cast<long double>(b) - a) / n;
+        long double sum = f(a) + f(b);
+
+        #pragma omp parallel for reduction(+ : sum)
+        for (int i = 1; i < n; ++i)
+        {
+            long double x = a + i * delta_x;
+
+            if (i % 2 == 0)
+            {
+                sum += 2 * f(x);
+            }
+            else
+            {
+                sum += 4 * f(x);
+            }
+        }
+
+        return (delta_x / 3) * sum;
+    }
+}
+
+#endif 
