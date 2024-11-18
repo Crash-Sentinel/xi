9c1802ee5a4464267931d6abe62e9f6252a0aff6
Crash Sentinel <151790578+Crash-Sentinel@users.noreply.github.com>
2024-11-18 15:45:25 -0500

    Update math_consts.h each var to uppercase
    Changed each var to uppercase to ensure clarity that these are constant variables and not intended to be modified.

--- include/math_consts.h
+++ include/math_consts.h
@@ -4,19 +4,19 @@
 class xi_math_consts
 {
     public:
-        inline static constexpr double h = 1e-5;
-        inline static constexpr double pi = 3.14159265358979323846;
-        inline static constexpr double half_pi = 1.57079632679;
-        inline static constexpr double third_pi = 1.0471975512;
-        inline static constexpr double quarter_pi = 0.78539816339;
-        inline static constexpr double sixth_pi = 0.52359877559;
-        inline static constexpr double phi = 1.61803398874989484820;
-        inline static constexpr double e = 2.71828182845904523536;
-        inline static constexpr double tau = 6.28318530717958647692;
-        inline static constexpr double gamma = 0.57721566490153286060651209008240243;
-        inline static constexpr double feigenbaum = 4.669201609102990671853203820446;
-        inline static constexpr double zeta = 1.2020569031595942854;
-        inline static constexpr double avogadro = 6.022e23;
+        inline static constexpr long double H = 3.29272e-10;
+        inline static constexpr long double PI = 3.14159265358979323846;
+        inline static constexpr long double HALF_PI = 1.57079632679;
+        inline static constexpr long double THIRD_PI = 1.0471975512;
+        inline static constexpr long double QUARTER_PI = 0.78539816339;
+        inline static constexpr long double SIXTH_PI = 0.52359877559;
+        inline static constexpr long double PHI = 1.61803398874989484820;
+        inline static constexpr long double E = 2.71828182845904523536;
+        inline static constexpr long double TAU = 6.28318530717958647692;
+        inline static constexpr long double GAMMA = 0.57721566490153286060651209008240243;
+        inline static constexpr long double FEIGENBAUM = 4.669201609102990671853203820446;
+        inline static constexpr long double ZETA = 1.2020569031595942854;
+        inline static constexpr long double AVOGADRO = 6.022e23;
 };
 
 #endif 
